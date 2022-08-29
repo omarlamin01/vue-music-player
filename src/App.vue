1851704a58ab2cba5b16948738ecc72b7d375d6f
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-08-29 19:29:14 +0000

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    //calculate sum of two int
+    sum(a, b) {
+      
+    }
   }
 }
 </script>
