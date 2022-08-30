4f9106fb75baa9693705cd246947e63cb656f359
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-08-30 16:34:08 +0000

    reformat code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div id="app">
-	<header>
-		<h1>Music player</h1>
-	</header>
-	<main>
-		<div class="player">
-			<h2 class="song-title"></h2>
-		</div>
-	</main>
-  </div>
+	<div id="app">
+		<header>
+			<h1>Music player</h1>
+		</header>
+		<main>
+			<div class="player">
+				<h2 class="song-title">Test</h2>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  methods: {},
+	name: "App",
+	methods: {},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+header {
+	display: flex;
+	justify-content: center;
 }
 </style>
